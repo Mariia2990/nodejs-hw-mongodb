09d7e88f7399392b7373969ac037ff4553e9c0d4
Mariia <masyakuk@gmail.com>
2025-01-20 11:17:01 +0200

    update 3 hw

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,39 @@
 import { ContactsCollections } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
-  return await ContactsCollections.find();
+  const contacts = await ContactsCollections.find();
+  return contacts;
 };
 export const getContactById = async (contactId) => {
-  return await ContactsCollections.findById(contactId);
+  const contact = await ContactsCollections.findById(contactId);
+  return contact;
+};
+export const createContact = async (payload) => {
+  const contact = await ContactsCollections.create(payload);
+  return contact;
+};
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactsCollections.findOneAndUpdate(
+    {
+      _id: contactId,
+    },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+  return rawResult;
+  // if (!rawResult || !rawResult.value) return null;
+  // return {
+  //   contact: rawResult.value,
+  //   isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  // };
+};
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollections.findOneAndDelete({
+    _id: contactId,
+  });
+  return contact;
 };
