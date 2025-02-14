b86e1b541cf848f31a7ed6fb3f40b08914c01576
Mariia <masyakuk@gmail.com>
2025-02-14 16:55:29 +0200

    add hw-7

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   loginUserSchema,
+  loginWithGoogleOAuthSchema,
   registerUserSchema,
   requestResetEmailSchema,
   resetPasswordSchema,
@@ -8,7 +9,9 @@ import {
 import { validateBody } from '../middlewares/validateBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
+  getGoogleOAuthUrlController,
   loginUserController,
+  loginWithGoogleController,
   logoutUserController,
   refreshUserSessionController,
   registerUserController,
@@ -18,20 +21,20 @@ import {
 
 const authRouter = Router();
 
-authRouter.post('/register', validateBody(registerUserSchema),ctrlWrapper(registerUserController),
-);
+authRouter.post('/register', validateBody(registerUserSchema),ctrlWrapper(registerUserController),);
 
-authRouter.post('/login',validateBody(loginUserSchema),ctrlWrapper(loginUserController),
-);
+authRouter.post('/login',validateBody(loginUserSchema),ctrlWrapper(loginUserController),);
 
 authRouter.post('/logout', ctrlWrapper(logoutUserController));
 
 authRouter.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
-authRouter.post('/send-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController),
-);
+authRouter.post('/send-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController),);
 
-authRouter.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController),
-);
+authRouter.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController),);
+
+authRouter.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+authRouter.post('/confirm-oauth', validateBody(loginWithGoogleOAuthSchema), ctrlWrapper(loginWithGoogleController),);
 
 export default authRouter;
